fb023558b75f191abbbb1589ff67b4cc35bf6803
Amin Fauzi <aremean0107@gmail.com>
2020-05-14 10:09:58 +0800

    Add files via upload

---
+++ aicp.sh
@@ -0,0 +1,3 @@
+
+# Fix home button wake device
+git -C frameworks/base fetch https://github.com/aminfauzi/aicp_frameworks_base q10.0 && git -C frameworks/base cherry-pick 4bd28797c8fc2ed66f630381e0abc4af9bd50ada

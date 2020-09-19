c5cd9e143d28c17ae52226d2dc3d877f35245b37
Amin Fauzi <aremean0107@gmail.com>
2020-09-19 16:35:43 +0800

    Delete superior.sh

--- superior.sh
+++
@@ -1,6 +0,0 @@
-
-# Fix build error
-git -C frameworks/av fetch https://github.com/aminfauzi/android_10_build_patch.git superior-frameworks-av && git -C frameworks/av cherry-pick b534c837635b2aca176bfbf126903e05c8809506 
-
-# Fix Miracast for devices using HDCP method
-git -C frameworks/av fetch https://github.com/aminfauzi/android_10_build_patch.git superior-frameworks-av && git -C frameworks/av cherry-pick 6cd12c14cc71c10c8b2eaf76de0294cbea690ea2
